04a48e02ff34a25baafeee5ea6e9e015abdbb95f
atman313 <atmanuyag@gmail.com>
2023-03-05 18:07:07 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
 import './App.css';
 import Carousel from './components/Carousel/Carousel';
 
 function App() {
   return (
     <div>
-      <h5 style={{textAlign: "center"}}>Carousel</h5>
-      <Carousel></Carousel>
+      <h5 style={{ textAlign: "center" }}>Carousel</h5>
+      <Carousel infinite timer={3500}>
+        <img src="/images/Sale1.jpg" alt="" />
+        <img src="/images/Store1.jpg" alt="" />
+        <img src="/images/Dress1.jpg" alt="" />
+        <img src="/images/Jeans1.jpg" alt="" />
+        <img src="/images/Shoes2.jpg" alt="" />
+        <img src="/images/Store2.jpg" alt="" />
+        <img src="/images/MensClothes1.jpg" alt="" />
+        <img src="/images/Shoes3.jpg" alt="" />
+        <img src="/images/WomensClothes1.jpg" alt="" />
+        <img src="/images/Shoes1.jpg" alt="" />
+      </Carousel>
     </div>
   );
 }
